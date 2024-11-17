ded5f4a7596cdbced11fecd4e7ef4c3e98a3ab40
KhantMinSan <sinetio150@gmail.com>
2024-11-18 02:29:23 +0800

    Test_bmi

--- Test_bmi.py
+++ Test_bmi.py
@@ -1,14 +1,14 @@
 import Lab2.bmi as bmi
 
 def test_bmi_normal_weight():
-    result =  bmi.calculate_bmi(57,1.73)
+    result =  bmi.calculate_bmi(1.73,57)
     assert result==0
 
 def test_bmi_over_weight():
-    result =  bmi.calculate_bmi(75,1.6)
+    result =  bmi.calculate_bmi(1.6,80)
     assert result==1
 
 def test_bmi_under_weight():
-    result =  bmi.calculate_bmi(40,1.73)
+    result =  bmi.calculate_bmi(1.73,40)
     assert result==-1
 
